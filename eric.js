c88ddf50c1034abe21953a9751ada457191cf71f
lexria <alexandriamlewis@me.com>
2016-02-26 08:30:49 -0500

    added files to work in unison

---
+++ eric.js
@@ -0,0 +1,5 @@
+//calvin's ajax example
+// https://github.com/TIY-Charleston-Front-End-February2016/demos/commit/5389c7bf2ef128f742b5e027ed5fa35adbb6f52e
+
+//hank's repo he's so nice
+// https://github.com/hankgielarowski/chatroom/blob/master/chatroom/main.js
