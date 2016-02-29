101c9c5eb683ada3526b9066bd5c7d7e4ec5cf59
lexria <alexandriamlewis@me.com>
2016-02-29 08:39:01 -0500

    just use main js it should work

--- alex.js
+++ alex.js
@@ -16,7 +16,7 @@ var chatroom = {
   url: 'http://tiny-tiny.herokuapp.com/collections/chattyPatty',
   init: function(){
     chatroom.initEvents();
-    chatroom.initStyling();
+    // chatroom.initStyling();
   },
 
   initEvents: function(){
@@ -27,7 +27,7 @@ var chatroom = {
   },
 
   initStyling: function(){
-    setInterval(function(){chatroom.getPost}, 200);
+    setInterval(function(){chatroom.getPosts}, 200);
   },
 
   submitUsername: function(event){
